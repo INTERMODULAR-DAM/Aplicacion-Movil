1b273ee05986325541257420d790f01090cbde5f
diego <diegofergo02@gmail.com>
2023-02-26 01:51:05 +0100

    Añadido el diseño de las cards de main a las del perfil del usuario

--- app/src/main/java/ejercicios/dam/intermodulardam/profile/ui/profileScreen.kt
+++ app/src/main/java/ejercicios/dam/intermodulardam/profile/ui/profileScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -19,7 +20,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -32,10 +35,14 @@ import ejercicios.dam.intermodulardam.main.domain.entity.Publication
 import ejercicios.dam.intermodulardam.main.domain.entity.User
 import ejercicios.dam.intermodulardam.Routes
 import ejercicios.dam.intermodulardam.main.ui.*
+import ejercicios.dam.intermodulardam.ui.theme.backgroundGreen
 import ejercicios.dam.intermodulardam.ui.theme.calibri
+import ejercicios.dam.intermodulardam.ui.theme.lightGreenCard
+import ejercicios.dam.intermodulardam.ui.theme.mediumGreenCard
 import ejercicios.dam.intermodulardam.utils.Constants.IP_ADDRESS
 import ejercicios.dam.intermodulardam.utils.MainBrown
 import ejercicios.dam.intermodulardam.utils.backgroundGreen
+import ejercicios.dam.intermodulardam.utils.standardQuadFromTo
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
@@ -76,13 +83,16 @@ fun PerfilScreen(navController: NavHostController, perfilViewModel: PerfilViewMo
         PerfilImage(user)
         Spacer(modifier = Modifier.height(10.dp))
         UserInfo(user, navController)
+        Spacer(modifier = Modifier.height(10.dp))
+        Row(Modifier.fillMaxWidth().padding(start = 5.dp), horizontalArrangement = Arrangement.Start) {Text(text = "Posts: ")}
+        Spacer(modifier = Modifier.height(5.dp))
         Row {
             LazyColumn(modifier = Modifier
                 .scrollable(scrollState, Orientation.Vertical)
                 .padding(bottom = 60.dp)) {
                 items(routes.size) { index ->
                     val postCreator =
-                        ProfileCards(navController, perfilViewModel, user, routes[index])
+                        ProfileCards(navController, user, routes[index])
                     Spacer(modifier = Modifier.height(5.dp))
                 }
             }
@@ -172,68 +182,6 @@ fun PerfilDrawer(navController: NavHostController, user:User, coroutineScope: Co
     }
 }
 
-@SuppressLint("SimpleDateFormat")
-@Composable
-fun ProfileCards(navController: NavHostController, perfilViewModel: PerfilViewModel, user:User, route:Publication) {
-    Card(modifier = Modifier
-        .padding(5.dp)
-        .fillMaxWidth()
-        .clip(RoundedCornerShape(10.dp))
-        .border(1.dp, MaterialTheme.colors.MainBrown, RoundedCornerShape(10.dp))
-        .clickable {
-            navController.navigate(
-                Routes.Publicacion.createRoute(
-                    route.id
-                )
-            )
-        },
-        elevation = 20.dp) {
-        Column(modifier = Modifier
-            .fillMaxSize()
-            .padding(5.dp)
-            .height(200.dp)){
-            RouteTitle(route.name, Modifier.align(Alignment.Start))
-            RouteCategory(route.category)
-            Divider(
-                Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 10.dp),
-                color = Color.LightGray)
-            Row(horizontalArrangement = Arrangement.SpaceBetween) {
-                Column(verticalArrangement =Arrangement.SpaceEvenly ,
-                    modifier = Modifier
-                        .align(Alignment.CenterVertically)
-                        .padding(vertical = 5.dp)) {
-                    Box(contentAlignment = Alignment.Center){
-                        RouteImage(route)
-                    }
-                    RouteUser(user, Modifier.align(Alignment.CenterHorizontally))
-                }
-
-                Divider(
-                    Modifier
-                        .fillMaxHeight()
-                        .width(1.dp)
-                        .padding(vertical = 10.dp))
-
-                Row(horizontalArrangement = Arrangement.SpaceEvenly) {
-                    Column(modifier = Modifier
-                        .align(Alignment.CenterVertically)
-                        .padding(horizontal = 10.dp)) {
-                        RouteParameter("Distance",route.distance)
-                        RouteParameter("Difficulty",route.difficulty)
-                    }
-                    Column(modifier = Modifier
-                        .align(Alignment.CenterVertically)) {
-                        RouteParameter("Duration",route.duration)
-                        RouteParameter("Date", SimpleDateFormat("dd/MM/yyyy").format(route.date) )
-                    }
-                }
-            }
-        }
-    }
-}
-
 @Composable
 fun PerfilImage(user:User) {
     Row{
@@ -257,6 +205,123 @@ fun UserInfo(user:User, navController: NavHostController) {
             .align(Alignment.CenterVertically))
         IconButton(onClick = { navController.navigate(Routes.EditProfile.createRoute(user.id)) }) {
             Icon(imageVector = Icons.Filled.Edit, contentDescription = "Editar Perfil")
+        }
+    }
+}
+
+@Composable
+fun ProfileCards(navController: NavHostController, user: User, route: Publication) {
+    Card(modifier = Modifier
+        .padding(5.dp)
+        .fillMaxWidth()
+        .border(4.dp, MaterialTheme.colors.MainBrown, CutCornerShape(20.dp, 0.dp, 20.dp, 0.dp))
+        .clip(CutCornerShape(20.dp, 0.dp, 20.dp, 0.dp))
+        .clickable {
+            navController.navigate(
+                Routes.Publicacion.createRoute(
+                    route.id,
+                )
+            )
+        },
+        elevation = 20.dp) {
+        BoxWithConstraints(modifier = Modifier
+            .background(backgroundGreen)
+            .padding(7.5.dp)
+            .aspectRatio(1f)
+            .clip(CutCornerShape(20.dp, 0.dp, 20.dp, 0.dp)))
+        {
+            val width = constraints.maxWidth
+            val height = constraints.maxHeight
+
+            val mediumColoredPoint1 = Offset(0f, height * 0.3f)
+            val mediumColoredPoint2 = Offset(width * 0.12f, height * 0.40f)
+            val mediumColoredPoint3 = Offset(width * 0.4f, height * 0.55f)
+            val mediumColoredPoint4 = Offset(width * 0.75f, height * 0.75f)
+            val mediumColoredPoint5 = Offset(width * 1.4f, - -height.toFloat() / 10f)
+
+            val mediumColoredPath = Path().apply {
+                moveTo(mediumColoredPoint1.x, mediumColoredPoint1.y)
+                standardQuadFromTo(mediumColoredPoint1, mediumColoredPoint2)
+                standardQuadFromTo(mediumColoredPoint2, mediumColoredPoint3)
+                standardQuadFromTo(mediumColoredPoint3, mediumColoredPoint4)
+                standardQuadFromTo(mediumColoredPoint4, mediumColoredPoint5)
+                lineTo(width.toFloat() + 100f, height.toFloat() + 100f)
+                lineTo(-100f, height.toFloat() + 100f)
+                close()
+            }
+
+            val lightPoint1 = Offset(0f, height * 0.8f)
+            val lightPoint2 = Offset(width * 0.1f, height * 0.85f)
+            val lightPoint3 = Offset(width * 0.3f, height * 0.90f)
+            val lightPoint4 = Offset(width * 0.65f, height * 0.75F)
+            val lightPoint5 = Offset(width * 1.4f, -height.toFloat() / 10f)
+
+            val lightColoredPath = Path().apply {
+                moveTo(lightPoint1.x, lightPoint1.y)
+                standardQuadFromTo(lightPoint1, lightPoint2)
+                standardQuadFromTo(lightPoint2, lightPoint3)
+                standardQuadFromTo(lightPoint3, lightPoint4)
+                standardQuadFromTo(lightPoint4, lightPoint5)
+                lineTo(width.toFloat() + 100f, height.toFloat() + 100f)
+                lineTo(-100f, height.toFloat() + 100f)
+                close()
+            }
+
+            Canvas(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                drawPath(
+
+                    path = mediumColoredPath,
+                    color = mediumGreenCard
+                )
+                drawPath(
+                    path = lightColoredPath,
+                    color = lightGreenCard
+                )
+            }
+            Column(modifier = Modifier
+                .fillMaxSize()
+                .padding(5.dp)
+                .height(250.dp)){
+                RouteTitle(route.name,
+                    Modifier
+                        .align(Alignment.Start)
+                        .padding(bottom = 10.dp))
+                RouteCategory(route.category)
+                Divider(
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 10.dp),
+                    color = Color.LightGray)
+                Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.align(Alignment.CenterHorizontally)) {
+                    Column(verticalArrangement =Arrangement.SpaceEvenly ,
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .padding(vertical = 5.dp)
+                            .width(150.dp)) {
+                        RouteImage(route)
+                        RouteUser(user, Modifier.align(Alignment.CenterHorizontally))
+                    }
+                    Divider(
+                        Modifier
+                            .fillMaxHeight()
+                            .width(1.dp)
+                            .padding(vertical = 10.dp)
+                            .background(Color.White))
+                    Column(
+                        verticalArrangement = Arrangement.SpaceEvenly, modifier = Modifier
+                            .fillMaxHeight()
+                            .padding(horizontal = 10.dp)
+                    ) {
+                        RouteParameter("Distance", route.distance)
+                        RouteParameter("Difficulty", route.difficulty)
+                        RouteParameter("Duration", route.duration)
+                        RouteParameter("Created at", SimpleDateFormat("dd/MM/yyyy").format(route.date))
+                    }
+                }
+            }
         }
     }
 }
